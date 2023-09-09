87ddb4e29f2b34354453aea842961f19ac6d2625
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-09-09 04:11:10 -0400

    technology text color is now default teal-400

--- personal-site-good/app/project_block.js
+++ personal-site-good/app/project_block.js
@@ -25,11 +25,11 @@ const Project_Block = ({project_name, project_description, technologies, project
         <a href={project_link} target="_blank" rel="noopener noreferrer">
             <div className={`rounded-xl p-4 grid-item h-96 sm:w-full w-80 hover:bg-white/5 hover:shadow-xl px-8 border-[2px] border-white/5 ${(selected === objects[project_name] || selected === 0) ? null:' opacity-40'}`}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
                 <div>
-                <p className={`font-bold text-xl font-poppins  ${(selected === objects[project_name]) ? 'text-teal-300':' text-white'}`}>{project_name}</p>
+                <p className={`font-bold text-xl font-poppins  ${(selected === objects[project_name]) ? 'text-teal-400':' text-white'}`}>{project_name}</p>
                 {/* <img src={require(project_image)} ></img> */}
                 <Image alt='' src={project_image} className="rounded-lg sm:my-8 my-4 border-2 border-black h-32 w-full object-cover"></Image>
                 <p className="h-16 font-inter sm:text-sm text-xs text-gray-400">{project_description}</p>
-                <p className={`font-thin font-inter sm:text-sm text-xs italic sm:mt-8 mt-12 ${(selected === objects[project_name]) ? 'text-teal-300':'text-white'}`}>{technologies}</p>
+                <p className={`font-thin font-inter sm:text-sm text-xs italic sm:mt-8 mt-12 ${(selected === objects[project_name]) ? 'text-teal-400':'text-teal-400'}`}>{technologies}</p>
                 </div>
 
                 {/* <p>{project_image}</p> */}
