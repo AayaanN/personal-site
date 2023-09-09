f1b025e4c52a198dfbae421e47cac67abbf38550
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-09-09 04:10:27 -0400

    integrated opacity decreasing into work.js, added my website as a project

--- personal-site-good/app/work/work.js
+++ personal-site-good/app/work/work.js
@@ -7,10 +7,13 @@ import Work_Block from "./work_block";
 
 const Work = () => {
 
+    const [selected, setSelected] = useState(0)
+
+
     return(
         <div className="sm:mt-20 text-white" id="experience">
             <h2 className="text-3xl font-bold font-poppins">Experience</h2>
-            <div className="rounded-xl ">
+            <div className="mt-12 rounded-xl ">
                 <Work_Block 
                     company='Florida International University - EPSi Group' 
                     title='Machine Learning Research Assistant' 
@@ -20,6 +23,20 @@ const Work = () => {
                     Also developed remotely deployable full stack application for battery forecasting.'
                     technologies='Python, Javascript, React, Tensorflow, Electron.'
                     link='https://eps.fiu.edu/'
+                    selected={selected}
+                    setSelected={setSelected}
+                    ></Work_Block>
+                
+                <Work_Block 
+                    company='Hack the Ridge' 
+                    title='Lead Organizer' 
+                    date='July 2021 - July 2022'
+                    description='Led a team of 8 executives, coordinating all event logistics for a local hackathon with 100+ participants.
+                    Oversaw website design, communicated with sponsors to secure $2000+ in sponsorships.'
+                    technologies='Javascript, React.'
+                    link='https://hacktheridge.ca'
+                    selected={selected}
+                    setSelected={setSelected}
                     ></Work_Block>
     
             </div>
